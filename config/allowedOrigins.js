e238503fc16a73231d152ca608dc269c8e0e3eb0
Adam Burrough <adamjamesburrough@gmail.com>
2023-05-24 19:04:05 +0900

    patch:cores

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8080/",
-  "https://vdms.onrender.com/",
+  "https://vdms.onrender.com",
 ];
 
 module.exports = allowedOrigins;
