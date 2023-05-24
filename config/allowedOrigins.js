ad7229b7cc0e59fa47ca08d5e7c4e5403656da06
Adam Burrough <adamjamesburrough@gmail.com>
2023-05-24 13:54:27 +0900

    patch: CORS UPDATE

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:8080/"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:8080/",
+  "https://vdms.onrender.com/",
+];
 
 module.exports = allowedOrigins;
