b035dbf3d0d63fe8cb50f7475b5ead6b26faf2f6
Adam Burrough <adamjamesburrough@gmail.com>
2023-05-31 17:04:10 +0900

    update:cors Origin list

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8080/",
   "https://vdms.onrender.com",
+  "https://www.combat-dragon.com",
 ];
 
 module.exports = allowedOrigins;
